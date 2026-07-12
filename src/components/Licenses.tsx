487fdb2773a1a135d89c6489c64182dba5e1f326
payam <payamsiami90@gmail.com>
2026-07-12 20:25:25 +0330

    fix bug

--- src/components/Licenses.tsx
+++ src/components/Licenses.tsx
@@ -37,14 +37,16 @@ export default function Licenses() {
 
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3 lg:gap-16">
           {CARDS.map((c, i) => (
-            <div key={i} className="flex w-full flex-col items-center gap-4 text-center">
-              <Image
-                src={c.icon}
-                alt=""
-                width={112}
-                height={112}
-                className="w-max h-max ml-auto"
-              />
+            <div key={i} className="flex w-full flex-col gap-4 text-center items-start">
+              <div className="w-40 h-20 text-start">
+                <Image
+                  src={c.icon}
+                  alt=""
+                  width={112}
+                  height={112}
+                  className="w-max h-max ml-auto"
+                />
+              </div>
               <div className="flex self-start">
                 <p className="font-bold text-[#12203f] sm:text-lg">{c.title}</p>
               </div>
@@ -63,7 +65,7 @@ export default function Licenses() {
                     <CornerIcon width={20} height={20} />
                     {c.ministry}
                   </span>
-                  {/* <span className="w-auto h-auto">
+                  <span className="w-5 h-5">
                     <Image
                       src="/assets/logo-mark.png"
                       alt=""
@@ -71,7 +73,7 @@ export default function Licenses() {
                       height={5}
                       className="h-auto w-auto flex object-cover flex-1"
                     />
-                  </span> */}
+                  </span>
                 </span>
               </div>
             </div>
